096c4ec90da8a813455d38043f40c1479eba646b
mkminson <mminson@byui.edu>
2023-09-18 13:10:31 -0600

    Finished prep 3 stretch assignments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,35 +9,53 @@ class Program
         //string userInput = Console.ReadLine();
         //int magicNumber = int.Parse(userInput);
 
-        //Instead of having the user supply the magic number, generate a random number from 1 to 100.
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1,101);
+        //declare variable to store user's answer as to if they want to play again
+        string answer;
 
-        //instantiate guess variable
-        int guess = 0;
+        //After the game is over, ask the user if they want to play again. Then, loop back and play the 
+        //whole game again and continue this loop as long as they keep saying "yes".
+        do 
+        {
+            //Instead of having the user supply the magic number, generate a random number from 1 to 100.
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-        //Add Do-While loop that keeps looping as long as the guess does not match the magic number.
-        do
-        {   //ask the user for a guess, read it in as a string, convert it to a number
-            Console.Write("What is your guess? ");
-            string guessInput = Console.ReadLine();
-            guess = int.Parse(guessInput);
+            //instantiate variable for useses guess
+            int guess = 0;
 
-            //Using an if statement, determine if the user needs to guess higher or lower next time, 
-            //or tell them if they guessed it.
+            //instantiate number of guesses variable
+            int numberOfGuesses = 0;
 
-            if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
+            answer = "";
+            //Add Do-While loop that keeps looping as long as the guess does not match the magic number.
+            do
+            {   //ask the user for a guess, read it in as a string, convert it to a number
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+
+                //Using an if statement, determine if the user needs to guess higher or lower next time, 
+                //or tell them if they guessed it.
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
                 else if (guess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
+                {
+                    Console.WriteLine("Higher");
+                }
                 else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        }   while (guess != magicNumber);     
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                numberOfGuesses++;
+            }   while (guess != magicNumber);   
+
+                //Keep track of how many guesses the user has made and inform them of it at the end of the game. 
+                Console.WriteLine($"You guessed the number in {numberOfGuesses} guesses.");
+
+                Console.Write("Do you want to play again? ");
+                answer = Console.ReadLine();
+        } while (answer == "yes");
     }
 }
