a001aedb9555ad16fa1cb1145111b9551bac4c0f
mkminson <mminson@byui.edu>
2023-09-26 16:12:27 -0600

    testing that job class works

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTItle = "Software Engineer";
+        job1._company = "Microsoft";
+        Console.WriteLine($"{job1._jobTItle} {job1._company}");
     }
 }
