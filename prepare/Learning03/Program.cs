2808f5f0173a31e33df090449445201811435fc3
mkminson <mminson@byui.edu>
2023-10-11 18:03:16 -0600

    Finished Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(2, 8);
+
+        f4.SetTop(1);
+        //Console.WriteLine(f4.GetTop());
+
+        f4.SetBottom(3);
+        //Console.WriteLine(f4.GetBottom());
+
+        Console.WriteLine(f1.GetFractionString());  
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());  
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());  
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());  
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
