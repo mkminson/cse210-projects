082c667eb6d82d79aa355689959ae7553b621230
mkminson <mminson@byui.edu>
2023-10-24 14:14:10 -0600

    Mindfulness menu

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Activity
+{
+    private string _activityName = "";
+    private string _activityDescription = "";
+    private int _activityDuration;
+    private string _finishingMessage = "";
+
+    // public string DisplayStartMessage()
+    // {
+    //     Console.WriteLine("");
+    // }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("    1. Start breathing activity");
+        Console.WriteLine("    2. Start reflecting activity");
+        Console.WriteLine("    3. Start listing activity");
+        Console.WriteLine("    4. Quit");
+        Console.Write("Select a choice from the menu: ");
+        
+        int option = int.Parse(Console.ReadLine());
+        if(option == 1)
+        {
+
+        }
     }
 }
